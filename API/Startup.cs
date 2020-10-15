8297322a6038bd28299f212c4cdec72758d13d35
Mithun Edappally <medappally@suyati.com>
2020-10-15 11:58:48 +0530

    React App and UI

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,8 +27,16 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options=>{
+            services.AddDbContext<DataContext>(options =>
+            {
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
             });
             services.AddControllers();
         }
@@ -43,7 +51,9 @@ namespace API
 
             // app.UseHttpsRedirection();
 
+
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
